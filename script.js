567917cb40d258080cbfa6c426b37b9e1ef80e88
aylton-dev <ayltono384@gmail.com>
2025-11-08 23:41:06 -0300

    https://github.com/Aylton-dev/Formulario-2.git

--- script.js
+++ script.js
@@ -42,3 +42,16 @@ document.getElementById('botao-cadastrar').addEventListener('click', function() 
 
 });
 
+document.getElementById('botao-entrar').addEventListener('click', function() {
+
+ 
+  const username = document.getElementById('username').value.trim();
+  const password = document.getElementById('password').value.trim();
+  
+
+    if(username === ''|| password === ''){
+        alert('❌ usuario e senha invalida!');
+    } 
+
+});
+
