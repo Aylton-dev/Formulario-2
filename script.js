c5b5fdaed6312dab6e30ff30d9ab7dc96ecabf2f
aylton-dev <ayltono384@gmail.com>
2025-11-08 22:13:07 -0300

    https://github.com/Aylton-dev/Formulario-2.git

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('id_nome_produto').addEventListener('keypress', function(event) {
+ 
+    const isSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(event.key);
+    
+    const isNumber = !isNaN(event.key) && event.key !== ' ';
+
+    if (isNumber && isSpecialChar){
+
+        event.preventDefault();{
+            alert('Este campo aceita apenas letras!');
+        }
+    }
+});
